052378ebfdb81746f20a54270a344a29d5bdee82
prathamesh-radical <prathmesh.n@radicalglobal.net>
2025-10-28 13:01:19 +0530

    bug fixed

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -2,6 +2,7 @@ import ProjectsPage from '../components/projectspage/ProjectsPage';
 import AndroidApps from '../components/projectspage/AndroidApps';
 import { MyContext } from '../Context/Context';
 import { useContext } from 'react';
+import ShopifyApps from '../components/projectspage/ShopifyApps';
 
 export default function Projects() {
     const { theme } = useContext(MyContext);
